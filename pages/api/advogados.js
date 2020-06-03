0adff74655ef9b91056e78f98b734f9ff1df8428
Bruno Henrique <bh.bsb2011@hotmail.com>
2020-06-02 22:02:45 -0300

    Login route and password encript

--- pages/api/advogados.js
+++ pages/api/advogados.js
@@ -1,17 +1,25 @@
 import knex from '../../src/database'
+import {hash} from 'bcrypt'
 
 export default async (req, res) => {
   if(req.method === 'GET'){
-    const db = await knex.select('id','email').from('advogados')
+    // Send all users  
+    const db = await knex.select('id','email','password').from('advogados')
     res.send(db)
   }
   if(req.method === 'POST'){
+    // register route
       try {
         const { email , password } = req.body
-        await knex('advogados').insert({
-          email,
-          password
+        // Hashing password
+        hash(password, 10, async (err, hash) => {
+          // Storing in the database
+          await knex('advogados').insert({
+            email: email,
+            password: hash
+          })
         })
+
         return res.status(201).send("Enviado com sucesso!")
       } catch (error) {
         return res.status(error)
---
+++ pages/api/advogados/login.js
@@ -0,0 +1,28 @@
+import knex from '../../../src/database'
+import { compare } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+
+export default async (req, res) => {
+  if(req.method === 'POST'){
+    // login route
+      try {
+        const { email , password } = req.body
+        const [user] = await knex('advogados').where({email}).select()
+
+        compare(password, user.password, (err, result) => {
+            if( !err && result){
+                const claims = {id: user.id, myEmail: user.email}
+                const jwt = sign(claims,'20c523ca-733d-4386-8691-33bdb3252636')
+                res.json({message: 'Logado com sucesso!', authToken: jwt})
+            }else{
+                res.json({error: 'Informacoes erradas'})
+            }
+        })
+
+      } catch (error) {
+        return res.status(error)
+      }
+  }else{
+    res.send('Não tem esse método.')
+  }
+}
